6b926ec1d7c88952fc2550bef2533624df509348
koringz <koringz>
2018-10-29 07:48:35 +0800

    from v2.0.2 to define tag and push v2.x

--- src/refs.js
+++ src/refs.js
@@ -2,6 +2,8 @@ const {
     forEach 
 } = require('./staticMethods.js')
 
+export const selfApi = ['onHeader','onBody','onFooter']
+
 /*
 需要在dialog body里面加载其他元素, 比如图片的失效 和 其他图片icon信息
 或者添加一条新的节点信息
@@ -16,8 +18,6 @@ const defineRefs = (self, child) => {
     });
     return obj;
 }
-
-export const selfApi = ['onHeader','onBody','onFooter']
 
 export const defaultRefs = (PROTO) => {
     selfApi.map((items) => {
