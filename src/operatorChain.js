2ab08f83b763dac3385027e0c564a74708c60213
koringz <260713986@qq.com>
2019-09-18 09:58:03 +0800

    修改operatorChain.js注释

--- src/operatorChain.js
+++ src/operatorChain.js
@@ -4,7 +4,6 @@ export default function operatorChain() {}
 
 /*
  * 打通`Coog`库外部和内部进行连接起来
- * 去掉`hide`和`show`方法
  * 并不是默认执行显示和隐藏,而是根据用户自定义设置`hide`和`show`实现需求功能.
  * 默认点击阴影部分会自动隐藏弹出框
  * Coog.app('.codialog').use({title: 'hello world! ^_^'})
